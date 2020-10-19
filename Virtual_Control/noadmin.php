035aeadfcee64210413e2bd75c86830d111d0fb7
ClearNB <clear.navy.blue.star@gmail.com>
2020-10-19 18:20:11 +0900

    files Commit

---
+++ Virtual_Control/noadmin.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="application-name" content="Virtual Control">
+  <link rel="icon" href="images/favicon.ico">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Virtual Control - A Controlling Network Tool.</title>
+  <meta name="description" content="Virtual Control - A Controlling Network Tool.">
+  <!-- CSS -->
+  <link rel="stylesheet" href="awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="aquamarine.css" type="text/css">
+  <!-- Javascript -->
+  <script src="js/navbar-ontop.js"></script>
+  <script src="js/animate-in.js"></script>
+  <script src="js/loader.js"></script>
+</head>
+
+<body>
+  <!-- Navbar & Logo -->
+  <div id="nav"></div>
+  <div class="bg-primary pt-5">
+    <div class="container">
+      <div id="logo"></div>
+    </div>
+  </div>
+  
+  <div class="py-3" style="">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12" style="">
+          <h2 class="" style=""><i class="fa fa-fw fa-exclamation-triangle"></i>アクセスできません</h2>
+          <p style="">このページは管理者権限 VCServer でログインしているユーザからのみアクセス可能です。<br></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bg-primary py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12"><a class="btn btn-block btn-lg btn-dark" href="index.php"><i class="fa fa-fw fa-arrow-circle-o-left"></i>戻る</a></div>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Footer -->
+  <div id="foot"></div>
+  
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script type="text/javascript">
+      load(1);
+  </script>
+</body>
+
+</html>
