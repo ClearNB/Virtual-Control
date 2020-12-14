e3f114a9fe2454c0935356c8da0d20460b74e280
ichinohe-a <66584987+ichinohe-a@users.noreply.github.com>
2020-12-14 18:50:03 +0900

    (Files Commit) Arashi Ichinohe 002

--- Virtual_Control/option/agent.php
+++ Virtual_Control/option/agent.php
@@ -4,67 +4,96 @@ include_once ('../scripts/session/session_chk.php');
 include_once ('../scripts/general/loader.php');
 include_once ('../scripts/general/former.php');
 
-if(!session_chk()) {
+if (!session_chk()) {
     http_response_code(301);
     header('Location: ../403.php');
     exit();
 }
 $id = $_SESSION['gsc_userid'];
 $getdata = select(true, "GSC_USERS", "USERNAME, PERMISSION", "WHERE USERID = '$id'");
-if(!$getdata && $getdata['PERMISSION'] != 0) {
+if (!$getdata && $getdata['PERMISSION'] != 0) {
     http_response_code(301);
     header('Location: ../403.php');
     exit();
 }
-
 $loader = new loader();
 
 $fm_pg = new form_generator('fm_pg');
+
+$fm_pg->Button('bt_ag_bk', '設定一覧へ', 'button', 'list');
 $fm_pg->SubTitle('OPTION - AGENT', 'エージェントを選択してください。', 'book');
 $fm_pg->Check(1, 'rd_01', 'agt', '1', 'AGENT1', true);
-$fm_pg->Check(1, 'rd_02', 'agt', '2', 'AGENT2',false);
-$fm_pg->Check(1, 'rd_03', 'agt', '3', 'AGENT3',false);
-$fm_pg->Button('bt_mk_ag', '作成', 'button','plus-square');
-$fm_pg->Button('bt_ed_ag', '編集', 'button','edit');
-$fm_pg->Button('bt_dl_ag', '削除', 'button','trash-alt');
+$fm_pg->Check(1, 'rd_02', 'agt', '2', 'AGENT2', false);
+$fm_pg->Check(1, 'rd_03', 'agt', '3', 'AGENT3', false);
+$fm_pg->Button('bt_ag_cr', '作成', 'button', 'plus-square');
+$fm_pg->Button('bt_ag_ed', '編集', 'button', 'edit');
+$fm_pg->Button('bt_ag_dl', '削除', 'button', 'trash-alt');
 
-$fm_ag_cr = new form_generator('fm_ag_cr');
-$fm_ag_ed = new form_generator('fm_ag_ed');
-$fm_ag_dl = new form_generator('fm_ag_dl');
+$fm_ag_cr = new form_generator('fm_ag_cr'); //エージェントIPアドレス
+//追加ページ
 
+$fm_ag_cr->Input('in_ag_ad', 'エージェントIPアドレス',
+        'IPアドレスのほか、ホスト名、ドメイン名の入力ができます。',
+        'server', true);
+
+$fm_ag_cr->Input('in_ag_co', 'コミュニティ名',
+        'SNMPv2cでのエージェントに対応したコミュニティ名を入力します。',
+        'american-sign-language-interpreting', true);
+
+$fm_ag_cr->Button('bt_pg_nx', '次へ', 'arrow-right');
+
+$fm_ag_sl = new form_generator('fm_ag_sl'); //MIBの設定
+
+
+
+
+$fm_ag_ed = new form_generator('fm_ag_ed'); //編集ページ
+
+
+
+
+
+$fm_ag_dl = new form_generator('fm_ag_dl'); //削除ページ
 ?>
 
 <html>
     <head>
-	<?php echo $loader->loadHeader('Virtual Control', 'OPTION - AGENT', true) ?>
-	<?php echo form_generator::ExportClass([$fm_pg, $fm_ag_cr, $fm_ag_ed, $fm_ag_dl]) ?>
+<?php echo $loader->loadHeader('Virtual Control', 'OPTION - AGENT', true) ?>
+        <?php echo form_generator::ExportClass([$fm_pg, $fm_ag_cr, $fm_ag_ed, $fm_ag_dl, $fm_ag_sl]) ?>
     </head>
     <body>
-	<?php echo $loader->navigation($getdata['PERMISSION']) ?>
-	<?php echo $loader->load_Logo() ?>
-	
-	<?php echo $loader->Title('OPTION - AGENT', 'user') ?>
-	<div id="data_output"></div>
-	
-	<?php echo $loader->footer() ?>
-	<?php echo $loader->footerS(true) ?>
-	
-	<script type="text/javascript">
-	    $(document).ready(function() {
-		animation('data_output', 0, fm_pg);
-	    });
-            
-             $(document).on('click', '#bt_mk_ag', function() { //追加ボタンクリック
-		animation('data_output', 400, fm_ag_cr);
-	    });
-            
-            $(document).on('click', '#bt_ed_ag', function() { //編集ボタンクリック
-		animation('data_output', 400, fm_ag_ed);
-	    });
-            
-            $(document).on('click', '#bt_dl_ag', function() { //削除ボタンクリック
-		animation('data_output', 400, fm_ag_dl);
-	    });
-	</script>
+<?php echo $loader->navigation($getdata['PERMISSION']) ?>
+        <?php echo $loader->load_Logo() ?>
+
+        <?php echo $loader->Title('OPTION - AGENT', 'user') ?>
+        <div id="data_output"></div>
+
+<?php echo $loader->footer() ?>
+        <?php echo $loader->footerS(true) ?>
+
+        <script type="text/javascript">
+            $(document).ready(function () {
+                animation('data_output', 0, fm_pg);
+            });
+
+            $(document).on('click', '#bt_ag_cr', function () { //追加ボタンクリック
+                animation('data_output', 400, fm_ag_cr);
+            });
+
+            $(document).on('click', '#bt_ag_ed', function () { //編集ボタンクリック
+                animation('data_output', 400, fm_ag_ed);
+            });
+
+            $(document).on('click', '#bt_ag_dl', function () { //削除ボタンクリック
+                animation('data_output', 400, fm_ag_dl);
+            });
+
+            $(document).on('click', '#bt_pg_nx', function () { //次へボタンクリック
+                animation('data_output', 400, fm_ag_sl);
+            });
+            $(document).on('click', '#bt_ag_bk', function () { //次へボタンクリック
+                animation_to_sites('data_output', 400, './');
+            });
+        </script>
     </body>
 </html>
